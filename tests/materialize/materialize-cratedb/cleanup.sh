3ef0dae60d554a9f72dbb87576e869d8fddbc039
surister <surister98@gmail.com>
2025-01-24 11:40:14 +0100

    Add first draft of CrateDB connector

---
+++ tests/materialize/materialize-cratedb/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Nothing to do because deleting the materialization also cleans up its tables.
+
+docker compose --file  ./materialize-cratedb/docker-compose.yaml down || true
